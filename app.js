aa286627bb3a9b1790ff7771db13666992b0657e
Daniel Feinblatt <daniel.feinblatt@duke.edu>
2020-05-02 12:22:53 -0400

    More Dynamic Navigation Bar

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150){
+		document.getElementById("topnav").style.paddingTop = "0px";
+	}
+	else {
+		document.getElementById("topnav").style.paddingTop = "20px";
+	}
+}
+
 function tester() {
 	document.getElementById("thisone").innerHTML = "how about now?";
 }
