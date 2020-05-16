b525f17a94cea0664f57859162c651e60f271d75
Daniel Feinblatt <daniel.feinblatt@duke.edu>
2020-05-16 17:18:23 -0400

    Globalized my navigation bar so all pages share it

---
+++ app.js
@@ -0,0 +1,14 @@
+var html = `
+	<ul class="navigation">
+		<li><a href="index.html">Home</a></li>
+		<li><a href="volunteer.html">Volunteering</a></li>
+		<li><a href="education.html">Education</a></li>
+		<li><a href="research.html">Research</a></li>
+		<li><a href="work.html">Work Experience</a></li>
+		<li><a href="athletics.html">Athletics</a></li>
+		<li><a href="#contact-me">Contact Me</a></li>
+		<li><a href="DanielFeinblattResume2020.pdf" target="_blank">Resume</a></li>
+	</ul>
+`;
+
+document.querySelector('nav').innerHTML = html
